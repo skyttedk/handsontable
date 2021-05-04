8a87036398cfcef9c147c45c95c3b486c44df901
Kim Sigurd Skytte <kss@relateit.dk>
2021-05-04 19:38:10 +0200

    update

--- js/window.factory.js
+++ js/window.factory.js
@@ -17,30 +17,8 @@ var WindowFactory = function () {
 				bodyStyle: "margin:0px",
 				resizable: true,
 				layout: "fit",
-				autoLoad: true, items: [
-					/*
-					{
-						title: "General",
-						html: "<h1>A simple tab</h1>",
-						style: "background-color:#df0000; ",
-					},
-					{
-						title: "Invoicing",
-						html: "Another one",
-					},
-					*/
-					{
-						title: "REmote",
-						loader: {  // Hmm vi kan lave noget loading stuff here
-							//autoLoad: true,
-							//useDefaultXhrHeader: true,
-							//url :"http://www.google.com"
-							//url: "wintest.html"
-							
-						}
-					}
-					
-				]
+				monitorResize: true,
+				autoLoad: true, 
 			});
 
 			//Create the window
@@ -58,34 +36,64 @@ var WindowFactory = function () {
 				layout: "fit",
 				scripts: true,
 				ready: false,
-				
 				listeners: {
 					boxready: async function (window, options) {
 						window.boxready = true
 						try {
-							
 							//window.onload();	
-							
 						} catch (error) {
-							
 							alert(error)
 						}
-						
-						
+					},
+					resize : (parentWindow) => {
+						//alert("mk")
+						//TODO: Resize panel content here
+						$('#dataGrid').width(parentWindow.width-20); //virker.. men find paretny
+						$('#dataGrid').height(parentWindow.height-20); //virker.. men find paretny
+
 					}
 				},
-				/*
+				//C:\Users\kss\Skrivebord\Nørd Ting\erpify\erpify.desktop\ext js\ext-5.1.0-gpl\ext-5.1.0\examples\kitchensink
+				//file:///C:/Users/kss/Skrivebord/N%C3%B8rd%20Ting/erpify/erpify.desktop/ext%20js/ext-5.1.0-gpl/ext-5.1.0/examples/menu/menus.js
 				tbar: [
 					{
-						text: "asd",
-						menu: []
+						text: "Window",
+						
+						menu: [
+							{
+								text:"Open File", 
+								icon: 'css/images/menu/default-checked.gif',
+								cls: 'x-btn-text-icon',
+								tooltip: '<b>Quick Tips</b><br/>Icon only button with tooltip<br><b>Activated on mousedown</b>',
+								handler: onMenuClick
+							},							{
+								text:"Calendar", 
+								icon: 'css/images/shared/calendar.gif',
+								cls: 'x-btn-text-icon',
+								tooltip: '<b>Quick Tips</b><br/>Icon only button with tooltip<br><b>Activated on mousedown</b>',
+								handler: onMenuClick
+							}
+						]
+					} , {
+						text: "Edit",
+						menu: ["Cut","Copy","Paste"]
+					}, {
+						//file:///C:/Users/kss/Skrivebord/N%C3%B8rd%20Ting/erpify/erpify.desktop/ext%20js/ext-5.1.0-gpl/ext-5.1.0/build/examples/simple-widgets/editor.html?theme=crisp-touch
+						text: "View",
+						menu: ["Form  View","",""]
+					}, {
+						text: "Data",
+						menu: ["Refresh","Import","Export"]
+					}, {
+						text: "Help",
+						menu: ["Help","About Erpify",]
 					}
 				],
-				*/
+			
 				//items: [pnl]
 			});
 
-
+	
 			var w = Ext.create('window.Debug', { renderTo: Ext.getBody() }).show();
 
 		
@@ -93,5 +101,9 @@ var WindowFactory = function () {
 
 		}
 	}
+}
+
+function onMenuClick() {
+	alert("Click")				
 }
 
